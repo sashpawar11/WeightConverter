2a2915787c3dd17cabdbc10125e013d7288e19c5
Saish <saisaipawar@gmail.com>
2021-08-29 20:07:08 +0530

    -Added UI Changes and Keyboard Hide Feature

--- app/src/main/java/com/sash/weightconverter/MainActivity.kt
+++ app/src/main/java/com/sash/weightconverter/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.sash.weightconverter
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -27,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         convertBtn = findViewById(R.id.convBtn)
         inputwt1 = findViewById(R.id.inputWt)
         isKg = true
+        inputwt1.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view,keyCode)}  // To Hide the keyboard after pressing enter on input Text field
         inputwt1.setHint("Enter Weight In Kilograms")
 
     }
@@ -80,6 +85,16 @@ class MainActivity : AppCompatActivity() {
             inputwt1.setHint("Enter Weight in Kilograms")
         }
 
-
+    }
+    // Function to Hide Keyboard on pressing Enter
+    private  fun handleKeyEvent(view: View, keyCode: Int): Boolean{
+        if(keyCode == KeyEvent.KEYCODE_ENTER)
+        {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 }
